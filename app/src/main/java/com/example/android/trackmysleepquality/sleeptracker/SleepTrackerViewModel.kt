a2edd8aa528cb5ce2235b625d697f8f674986683
aditya <aditya003.ay@gmail.com>
2021-12-15 23:46:18 +0530

    Displaying the data using Transformations

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -20,8 +20,10 @@ import android.app.Application
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
 import kotlinx.coroutines.*
 
 /**
@@ -47,6 +49,10 @@ class SleepTrackerViewModel(
 
         // A variable for all nights
         private val nights = database.getAllNights()
+
+        val nightsString = Transformations.map(nights){ nights ->
+                formatNights(nights, application.resources)
+        }
 
         init{
                 initializeTonight()
